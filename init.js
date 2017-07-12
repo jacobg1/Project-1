c247464a186596a6f26b9639e9618d193bda8fbc
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-11 20:15:35 -0500

    seperate pages

--- init.js
+++ init.js
@@ -1,5 +1,10 @@
-// $(document).ready(function () {
-//   const game = new SimonModel()  // create game construct
-//   const newGame = new SimonView(game)  // create new view construct and pass game as argument
-//   newGame.init()  // call the init method on the newGame construct
-// })
+$(document).ready(function () {
+  const game = new SimonModel()  // create game construct
+  const newGame = new SimonView(game)  // create new view construct and pass game as argument
+  newGame.init()  // call the init method on the newGame construct
+  console.log(newGame);
+  console.log(game);
+  console.log(game.simonPattern);
+  game.setSimonPattern()
+  console.log(game.simonPattern);
+})
